4aad4c26d9403af41475b8dde85518c66218cd57
camalzadeelvin <camalzadeelvin@STHQ0122-01>
2023-04-25 21:44:06 +0400

    Add project files.

---
+++ App.Entities/Concrete/Product.cs
@@ -0,0 +1,23 @@
+﻿using App.Core.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public int CategoryID { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+        [Required]
+        public short UnitsInStock { get; set; }
+    }
+}
